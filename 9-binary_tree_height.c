810c6677e7c4928f426e78b873902037a1cfb98b
starkjoy <residualcode6@gmail.com>
2023-05-31 11:49:00 +0000

    Corrected some bugs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
