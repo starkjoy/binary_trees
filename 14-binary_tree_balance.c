9a7b9fa4d9ea3552939b68b1f0de5a15cfae602b
Joy <117626913+starkjoy@users.noreply.github.com>
2023-05-31 12:08:39 +0000

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,8 +6,6 @@
  * Return: balance factor
  */
 
-size_t binary_tree_height(const binary_tree_t *tree);
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int balance_factor;
@@ -25,23 +23,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (balance_factor);
 }
 
+#include "binary_trees.h"
+
 /**
- * binary_tree_height - gets the height of a binary tree
- * @tree: root node to draw height from for tree
- * Return: size_t representing height, 0 on failure or NULL.
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to root node
+ * Return: height of binary tree
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_size = 0;
-	size_t right_size = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	left_size += 1 + binary_tree_height(tree->left);
-	right_size += 1 + binary_tree_height(tree->right);
-	if (left_size > right_size)
-		return (left_size);
+	if (tree->left)
+		left_height += 1 + binary_tree_height(tree->left);
 
-	return (right_size);
+	if (tree->right)
+		right_height += 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
